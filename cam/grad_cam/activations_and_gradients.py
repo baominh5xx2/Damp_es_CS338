4403f7533875dd93fd41fba1a5fe00f09bf8b348
baominh5xx2 <baominh5xxx2@gmail.com>
2026-05-13 15:43:00 +0700

    Guard GradCAM hooks during feature extraction

--- cam/grad_cam/activations_and_gradients.py
+++ cam/grad_cam/activations_and_gradients.py
@@ -4,6 +4,9 @@ class ActivationsAndGradients:
         self.gradients = []
         self.activations = []
         self.reshape_transform = reshape_transform
+        self.height = None
+        self.width = None
+        self.recording = False
         self.handles = []
         for target_layer in target_layers:
             self.handles.append(
@@ -12,18 +15,28 @@ class ActivationsAndGradients:
                 target_layer.register_forward_hook(self.save_gradient))
 
     def save_activation(self, module, input, output):
+        if not self.recording:
+            return
+
         activation = output
 
         if self.reshape_transform is not None:
+            if self.height is None or self.width is None:
+                return
             activation = self.reshape_transform(activation, self.height, self.width)
         self.activations.append(activation.cpu().detach())
 
     def save_gradient(self, module, input, output):
+        if not self.recording:
+            return
+
         if not hasattr(output, "requires_grad") or not output.requires_grad:
             return
 
         def _store_grad(grad):
             if self.reshape_transform is not None:
+                if self.height is None or self.width is None:
+                    return
                 grad = self.reshape_transform(grad, self.height, self.width)
             self.gradients = [grad.cpu().detach()] + self.gradients
 
@@ -34,10 +47,13 @@ class ActivationsAndGradients:
         self.width = W // 16
         self.gradients = []
         self.activations = []
-        if isinstance(x, list):
-            return self.model.forward_last_layer(x[0], x[1])
-        else:
+        self.recording = True
+        try:
+            if isinstance(x, list):
+                return self.model.forward_last_layer(x[0], x[1])
             return self.model(x)
+        finally:
+            self.recording = False
 
     def release(self):
         for handle in self.handles:
